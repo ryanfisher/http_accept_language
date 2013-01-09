0bf8415c11d317903c37c71699f9f5383848f475
Ryan Fisher <ryancfisher@gmail.com>
2013-01-09 10:02:25 -0800

    Use subject method for idiomatic rspec specs

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,55 +2,53 @@ require 'http_accept_language/parser'
 
 describe HttpAcceptLanguage::Parser do
 
-  def parser
-    @parser ||= HttpAcceptLanguage::Parser.new('en-us,en-gb;q=0.8,en;q=0.6,es-419')
-  end
+  subject { described_class.new('en-us,en-gb;q=0.8,en;q=0.6,es-419') }
 
   it "should return empty array" do
-    parser.header = nil
-    parser.user_preferred_languages.should eq []
+    subject.header = nil
+    subject.user_preferred_languages.should eq []
   end
 
   it "should properly split" do
-    parser.user_preferred_languages.should eq %w{en-US es-419 en-GB en}
+    subject.user_preferred_languages.should eq %w{en-US es-419 en-GB en}
   end
 
   it "should ignore jambled header" do
-    parser.header = 'odkhjf89fioma098jq .,.,'
-    parser.user_preferred_languages.should eq []
+    subject.header = 'odkhjf89fioma098jq .,.,'
+    subject.user_preferred_languages.should eq []
   end
 
   it "should properly respect whitespace" do
-    parser.header = 'en-us, en-gb; q=0.8,en;q = 0.6,es-419'
-    parser.user_preferred_languages.should eq %w{en-US es-419 en-GB en}
+    subject.header = 'en-us, en-gb; q=0.8,en;q = 0.6,es-419'
+    subject.user_preferred_languages.should eq %w{en-US es-419 en-GB en}
   end
 
   it "should find first available language" do
-    parser.preferred_language_from(%w{en en-GB}).should eq "en-GB"
+    subject.preferred_language_from(%w{en en-GB}).should eq "en-GB"
   end
 
   it "should find first compatible language" do
-    parser.compatible_language_from(%w{en-hk}).should eq "en-hk"
-    parser.compatible_language_from(%w{en}).should eq "en"
+    subject.compatible_language_from(%w{en-hk}).should eq "en-hk"
+    subject.compatible_language_from(%w{en}).should eq "en"
   end
 
   it "should find first compatible from user preferred" do
-    parser.header = 'en-us,de-de'
-    parser.compatible_language_from(%w{de en}).should eq 'en'
+    subject.header = 'en-us,de-de'
+    subject.compatible_language_from(%w{de en}).should eq 'en'
   end
 
   it "should accept symbols as available languages" do
-    parser.header = 'en-us'
-    parser.compatible_language_from([:"en-HK"]).should eq :"en-HK"
+    subject.header = 'en-us'
+    subject.compatible_language_from([:"en-HK"]).should eq :"en-HK"
   end
 
   it "should sanitize available language names" do
-    parser.sanitize_available_locales(%w{en_UK-x3 en-US-x1 ja_JP-x2 pt-BR-x5}).should eq ["en-UK", "en-US", "ja-JP", "pt-BR"]
+    subject.sanitize_available_locales(%w{en_UK-x3 en-US-x1 ja_JP-x2 pt-BR-x5}).should eq ["en-UK", "en-US", "ja-JP", "pt-BR"]
   end
 
   it "should find most compatible language from user preferred" do
-    parser.header = 'ja,en-gb,en-us,fr-fr'
-    parser.language_region_compatible_from(%w{en-UK en-US ja-JP}).should eq "ja-JP"
+    subject.header = 'ja,en-gb,en-us,fr-fr'
+    subject.language_region_compatible_from(%w{en-UK en-US ja-JP}).should eq "ja-JP"
   end
 
 end
